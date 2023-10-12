40cc7216b7b12d552954cb1d3d814ab5441e8fb2
Kozoaku <93962386+Kozoaku@users.noreply.github.com>
2023-10-12 10:11:46 -0400

    Add Currency module shell

--- clock/core.lua
+++ clock/core.lua
@@ -2,7 +2,7 @@ local _, ns = ...
 
 ---@class XIV_Databar: AceAddon
 local addon = LibStub("AceAddon-3.0"):GetAddon("XIV_Databar")
----@class XIV_Databar_Clockr: AceModule, AceConsole-3.0
+---@class XIV_Databar_Clock: AceModule, AceConsole-3.0
 local module = addon:NewModule("XIV_Databar_Clock")
 
 function module:OnInitialize()
