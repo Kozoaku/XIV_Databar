566e98a6e294176b485ab1fa1717d2dd2e2d6a2e
Kozoaku <93962386+Kozoaku@users.noreply.github.com>
2023-10-12 10:05:09 -0400

    Fix lua warnings

--- armor/core.lua
+++ armor/core.lua
@@ -1,6 +1,9 @@
 local _, ns = ...
+
+---@class XIV_Databar: AceAddon, AceConsole-3.0
 local addon = LibStub("AceAddon-3.0"):GetAddon("XIV_Databar")
-local module = addon:NewModule("XIV_Databar_Armor")
+---@class XIV_Databar_Armor: AceModule, AceConsole-3.0
+local module = addon:NewModule("XIV_Databar_Armor", "AceConsole-3.0")
 
 function module:OnInitialize()
     self:Print("XIV_Databar_Armor OnInitialize function")
