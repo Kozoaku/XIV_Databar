8e475954920d593c512c8f6626e0150a8ab2316d
Kozoaku <93962386+Kozoaku@users.noreply.github.com>
2023-10-12 10:07:18 -0400

    Addition lua warning fixes

--- armor/core.lua
+++ armor/core.lua
@@ -1,6 +1,6 @@
 local _, ns = ...
 
----@class XIV_Databar: AceAddon, AceConsole-3.0
+---@class XIV_Databar: AceAddon
 local addon = LibStub("AceAddon-3.0"):GetAddon("XIV_Databar")
 ---@class XIV_Databar_Armor: AceModule, AceConsole-3.0
 local module = addon:NewModule("XIV_Databar_Armor", "AceConsole-3.0")
--- clock/core.lua
+++ clock/core.lua
@@ -1,5 +1,8 @@
 local _, ns = ...
+
+---@class XIV_Databar: AceAddon
 local addon = LibStub("AceAddon-3.0"):GetAddon("XIV_Databar")
+---@class XIV_Databar_Clockr: AceModule, AceConsole-3.0
 local module = addon:NewModule("XIV_Databar_Clock")
 
 function module:OnInitialize()
